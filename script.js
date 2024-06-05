9a6c823103f8a2ad41945a492006a334c1ffe6be
SAI KIRAN <40574253+vepamanisaikiran@users.noreply.github.com>
2024-06-05 10:44:27 +0000

    updated script file

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-  faceapi.nets.ageGenderNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/Face-Detection-JavaScript/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/Face-Detection-JavaScript/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/Face-Detection-JavaScript/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/Face-Detection-JavaScript/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/Face-Detection-JavaScript/models')
 ]).then(startVideo)
 
 function startVideo() {
