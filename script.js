8cfcf1c89d33ee7a7d9d07ebf5f3dfbbf0ab2983
SAI KIRAN <40574253+vepamanisaikiran@users.noreply.github.com>
2024-06-05 10:30:02 +0000

    updated for age

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -21,11 +22,25 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+    console.log(detections)
+  //   if(detections.length() > 0){
+  //   faceapi.draw.drawDetections(detections, detections.map(res => res.detection))
+  //   results.forEach(result => {
+  //     const { age, gender, genderProbability } = result
+  //     new faceapi.draw.DrawTextField(
+  //       [
+  //         `${faceapi.utils.round(age, 0)} years`,
+  //         `${gender} (${faceapi.utils.round(genderProbability)})`
+  //       ],
+  //       result.detection.box.bottomLeft
+  //     ).draw(out)
+  //   })
+  // }
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+  }, 1000)
 })
